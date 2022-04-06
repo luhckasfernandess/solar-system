47794dc5f19714033ef3ca98b879311dd9994d29
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-04-06 16:45:12 -0400

    Requirement 6 complete! :heavy_check_mark: Solar System rendered

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((pl) => (
+          <PlanetCard key={ pl.name } planetName={ pl.name } planetImage={ pl.image } />
+        ))}
       </div>
     );
   }
